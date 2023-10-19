0d7c1d6408bc2141bfacadf564f6fe7c52efab8e
Roshan Binoj <roshan23bcs9@iiitkottayam.ac.in>
2023-10-19 15:38:28 +0530

    Print fibonacci using recursion

---
+++ C/20th Submit/fib.c
@@ -0,0 +1,22 @@
+/*WAP in C to print the fibonacci series using recursion*/
+#include <stdio.h>
+int fib(int n) {
+    if (n <= 1) {
+        return n;
+    } else {
+        return (fib(n - 1) + fib(n - 2));
+    }
+}
+
+int main()
+{
+    int n;
+    printf("Enter the number of terms for the Fibonacci series: ");
+    scanf("%d", &n);
+    printf("Fibonacci Series up to %d terms:\n", n);
+    for (int i = 0; i < n; i++) {
+            printf("%d ", fib(i));
+        }
+        printf("\n");
+    return 0;
+}
