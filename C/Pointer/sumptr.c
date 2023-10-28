fb10647f7d19966151f8db0f4bcd35b5247217bd
Roshan Binoj <roshan23bcs9@iiitkottayam.ac.in>
2023-10-28 10:08:07 +0530

    Find the sum of numbers in array using pointer

---
+++ C/Pointer/sumptr.c
@@ -0,0 +1,17 @@
+/*A program to find the sum of numbers in an array usimng pointer*/
+#include <stdio.h>
+
+int main()
+{
+    int a[3] = {1, 2, 3};
+    int *p = a;
+    int sum = 0;
+
+    for (int i = 0; i < 3; i++) {
+        sum += *p;
+        p++;
+    }
+
+    printf("Sum of elements = %d", sum);
+    return 0;
+}
