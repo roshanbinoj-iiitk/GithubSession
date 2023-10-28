9955b87d4e064043920838ea52c2d625709d14f0
Roshan Binoj <roshan23bcs9@iiitkottayam.ac.in>
2023-10-28 09:52:00 +0530

    Access elements of array using pointers

---
+++ C/Pointer/acss.c
@@ -0,0 +1,13 @@
+/*Access elements of array using pointers*/
+#include <stdio.h>
+int main()
+{
+    int a[3]={1,2,3};
+    int *p=a;
+    for(int i=0;i<3;i++)
+    {
+        printf("%d ", *p);
+        p++;
+    }
+    return 0;
+}
